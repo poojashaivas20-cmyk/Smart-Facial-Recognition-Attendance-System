6da8476933a8d62a6007fbda8212825e7030e193
pooja <poojashaivas20@gmail.com>
2026-04-18 11:30:04 +0530

    fix: secure credentials, fix delete bug, add date/time to attendance, fix date filter, add email alerts

--- config.py
+++ config.py
@@ -1,13 +1,16 @@
 import os
 import urllib.parse
+from dotenv import load_dotenv
+
+# Load .env file automatically
+load_dotenv()
 
 class Config:
     # MySQL Configuration
     MYSQL_HOST = os.getenv('MYSQL_HOST', 'localhost')
     MYSQL_USER = os.getenv('MYSQL_USER', 'root')
     
-    # We encode the password to handle special characters like '@'
-    raw_password = 'Prabhu@18'
+    raw_password = os.getenv('MYSQL_PASSWORD', '')
     MYSQL_PASSWORD = urllib.parse.quote_plus(raw_password)
     
     MYSQL_DB = os.getenv('MYSQL_DB', 'sfras_db')
@@ -18,8 +21,7 @@ class Config:
     SECRET_KEY = os.urandom(24)
     
     # Email Service Configuration
-    # NOTE: Use an "App Password" (not your normal password) for Gmail
     MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 465
-    MAIL_USERNAME = os.getenv('MAIL_USERNAME', 'poojapooju2645@gmail.com')
-    MAIL_PASSWORD = os.getenv('MAIL_PASSWORD', 'hyipvkhwarzoeteh')
+    MAIL_USERNAME = os.getenv('MAIL_USERNAME', '')
+    MAIL_PASSWORD = os.getenv('MAIL_PASSWORD', '')
